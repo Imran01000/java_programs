7965d5d732e365a7f72be5596d1c830f9cf2e1c7
Imran01000 <imshaik01000@gmail.com>
2020-02-27 15:49:07 +0530

    ADDED:Gambler make 1$ for bet so either win or loss.

--- Gambling/src/Gambler.java
+++ Gambling/src/Gambler.java
@@ -1,12 +1,29 @@
 import java.util.*;
 public class Gambler {
-	int stake=100;
-	int bet=1;
+	static int stake=100;
+	static int bet=1;
 	public static void main(String[] args) {
+		
 		Gambler g=new Gambler();
-		System.out.println("Hello gambler");
-		System.out.println("Stake value"+g.stake);
-		System.out.println("Bet value"+g.bet);
+		Random r=new Random();
+		System.out.println(stake);
+		while(true)
+		{
+			int played=r.nextInt(2);
+			if(played==1)
+			{
+				System.out.println("You win bet");
+				stake++;	
+				break;
+			}
+			else
+			{
+				System.out.println("you loss bet");
+				stake=stake-bet;
+				break;
+			}
+		}
+		System.out.println("Current stake condition : "+stake);
 	}
 	
 }
