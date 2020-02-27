f1149c4f2e264a9937f6073e84dc7e1d6d89765c
Imran01000 <imshaik01000@gmail.com>
2020-02-27 14:34:16 +0530

    ADDED:Taking stake as 100$ and bet as .

---
+++ Gambling/src/Gambler.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Gambler {
+	int stake=100;
+	int bet=1;
+	public static void main(String[] args) {
+		Gambler g=new Gambler();
+		System.out.println("Hello gambler");
+		System.out.println("Stake value"+g.stake);
+		System.out.println("Bet value"+g.bet);
+	}
+	
+}
