edf4bc2d1b6a6a56b1d12703cdeb8d7bc32c3ac3
Imran01000 <imshaik01000@gmail.com>
2020-02-28 11:11:36 +0530

    MODIFIED:Showing total win and loss amount .

--- Gambling/src/Gambler.java
+++ Gambling/src/Gambler.java
@@ -3,45 +3,56 @@ public class Gambler {
 	static int won=0;
 	static int loss=0;
 	static int stake=100;
+	static int stakeTemp=stake;
 	static int bet=1;
-	final static int full=150;
-	final static int half=50;
-	public static void main(String[] args) {
+	static int half=stake/2;
+	static int cash=0;
+	static int count1=0;
+	static int count0=0;
+	static int winMoney=0;
+	static int lossMoney=0;
+	static int arrayDays[]=new int[20];
+		public static void main(String[] args) {
 		
-		Gambler g=new Gambler();
 		Random r=new Random();
 		System.out.println(stake);
-		for(int days=1;days<=20;days++)
+		for(int days=0;days<20;days++)
 		{	
-			while(true)
+			int played=r.nextInt(2);
+			if(played==1)
 			{
-				int played=r.nextInt(2);
-				if(played==1)
-				{
-					System.out.println("You win bet");
-					stake++;
-					if(stake==full)
-					{
-						won=won+50;
-						break;
-					}
+				System.out.println("You win bet");
+				stakeTemp=stakeTemp+half;
+				System.out.println(stakeTemp);
+				count1++;
 				
-				}
-				else
-				{
-					System.out.println("you loss bet");
-					stake=stake-bet;
-					if(stake==half)
-					{
-						loss=loss-50;
-						break;
-					}
-				
-				}
+										
 			}
-		System.out.println("Current stake condition : "+stake);
-		System.out.println(won);
-		System.out.println(loss);
+			else
+			{
+				System.out.println("you loss bet");
+				stakeTemp=stakeTemp-half;
+				System.out.println(stake);
+				count0++;
+				
+			}
+			arrayDays[days]=stakeTemp;
+			
 		}
+			System.out.println("Number of times win : "+count1);
+			System.out.println("Number of times loss : "+count0);
+			winMoney=count1*half;
+			lossMoney=count0*half;
+			for (int i = 0; i <arrayDays.length; i++) {
+				System.out.print(arrayDays[i]+" ");
+			}
+			System.out.println();
+			System.out.println("Total win amount = "+winMoney);
+			System.out.println("Total loss amount = "+lossMoney);
+			
 	}
-}
+		
+		
+}	
+
+
